c7fa08a8fafc23927c068896056181c2c4064028
mmadiwal <65014199+mmadiwal@users.noreply.github.com>
2020-05-19 17:54:43 +0530

    Test cases changes

--- Xray/src/test/java/com/xpand/DemoTest.java
+++ Xray/src/test/java/com/xpand/DemoTest.java
@@ -51,57 +51,23 @@ public class DemoTest {
 		  driver.quit();
 		  }
  
-    @Test
-    @Xray(test ="XSD-2")
-    public void loginTest() {
-		  HomePage hp=new HomePage();
-			Properties prop=hp.getProperties();
-			driver=hp.initialization();
-		  //Enter the UserName 
-		driver.findElement(By.name("email")).sendKeys(prop.getProperty("username"));
-		//Enter the password
-		driver.findElement(By.name("password")).sendKeys(prop.getProperty("password"));
-		//Click on the Login Button
-		driver.findElement(By.xpath("//div[contains(@class,'ui fluid large blue submit button')]")).click();
-		WebDriverWait wait = new WebDriverWait(driver, 30);
-		String dealsText=wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//a//span[contains(text(),'Deals')]"))).getText();
-	    Assert.assertEquals(dealsText, "Deals");
-	    System.out.println("Successfully able to login to the application ");
-	    driver.quit();
-	 	
-}    
-	
-	  @Test
 	  
 	  @Xray(test = "XSD-9") public void verifyUser() {
-	 HomePage hp=new HomePage();
-	  Properties prop=hp.getProperties(); driver=hp.initialization();
-	  driver.findElement(By.name("email")).sendKeys(prop.getProperty("username"));
-	  //Enter the password
-	  driver.findElement(By.name("password")).sendKeys(prop.getProperty("password"));
-	  //Click on the Login Button 
-	  driver.findElement(By.xpath("//div[contains(@class,'ui fluid large blue submit button')]")).click();
-	  WebDriverWait wait = new WebDriverWait(driver,40); 
-	  String userNme=wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//div//span[contains(text(),'Mahant Madiwal')]"))).getText();
-	  //Comparing both the texts 
-	  Assert.assertEquals(userNme, "Mahant Madiwal");
+	  System.setProperty("webdriver.chrome.silentOutput", "true");
+				System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver.exe");
+				driver = new ChromeDriver();
+				driver.manage().window().maximize();
+				//driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
+				driver.manage().timeouts().implicitlyWait(80, TimeUnit.SECONDS);
+				driver.get("https://ui.freecrm.com/");
+				String title=driver.getTitle();
+				WebDriverWait wait = new WebDriverWait(driver, 60);
+				wait.until(ExpectedConditions.titleContains(title));
+				System.out.println(driver.getCurrentUrl()) ;
 	  System.out.println("Verified user successfully"); 
 	  //Closing the browser
 	  driver.quit(); }
 	 
     
-	/*
-	 * @Test public void verifyDocumentsTab() { HomePage hp=new HomePage();
-	 * Properties prop=hp.getProperties(); driver=hp.initialization();
-	 * driver.findElement(By.name("email")).sendKeys("madiwal.m@gmail.com"); //Enter
-	 * the password
-	 * driver.findElement(By.name("password")).sendKeys("Alti@33533533"); //Click on
-	 * the Login Button driver.findElement(By.
-	 * xpath("//div[contains(@class,'ui fluid large blue submit button')]")).click()
-	 * ; //Navigate to the documents tab boolean
-	 * flag=driver.findElement(By.xpath("//span[contains(text(),'Documents')]")).
-	 * isDisplayed(); Assert.assertTrue(flag);
-	 * System.out.println("Verified Home Tab Successfully"); //Closing the browser
-	 * driver.quit(); }
-	 */
+	
 }
