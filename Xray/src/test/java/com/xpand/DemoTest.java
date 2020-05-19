f42f6b9bf99442047dc1ad185ebb614625371532
mmadiwal <65014199+mmadiwal@users.noreply.github.com>
2020-05-19 18:58:14 +0530

    Update DemoTest.java

--- Xray/src/test/java/com/xpand/DemoTest.java
+++ Xray/src/test/java/com/xpand/DemoTest.java
@@ -10,6 +10,7 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.AfterSuite;
 
